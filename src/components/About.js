402db9551afaaa387f78f2a86594333780f2f1ed
Zakir Butte <zakir.butte@gmailcom>
2018-03-09 15:44:49 -0800

    added weather

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+const axios = require('axios');
 
-const About = props => {
-  return (
-    <div>
-      <h1>About</h1>
-      <p>1-800-ABOUT-ME</p>
-      <p>1506 NE 53rd St<br/>
-      Seattle, WA 98105</p>
-    </div>
-  )
+class About extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      zipcode:'',
+      weather:''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  // componentDidMount() {
+  //
+  // }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let zip = this.state.zipcode;
+    let url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&appid=052f26926ae9784c2d677ca7bc5dec98&units=imperial";
+    axios({
+      method: 'GET',
+      url
+    })
+    .then((response) => {
+      console.log(response.data)
+      this.setState({
+        weather:response.data
+      })
+    })
+    .catch((error) => {
+      console.log('An error occured while parsing!', error)
+    })
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({
+      zipcode: e.target.value
+    })
+  }
+
+  render () {
+
+    const {weather} = this.state
+    if (weather) {
+      return (
+        <div>
+          <h2>{weather.name}</h2>
+          <p>{weather.weather[0].main}</p>
+          <div>{Math.round(weather.main.temp)} &deg; F</div>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h1>Weather stuff</h1>
+          <p>where some weather info might go</p>
+          <input type='text' value={this.state.zipcode} onChange={(e) => this.handleChange(e)} />
+          <button type='submit' onClick={(e) => this.handleSubmit(e)}>Look up Weather</button>
+        </div>
+      )
+    }
+  }
 }
 
 export default About;
